4feba97186d2bfed65c7896f55cd66a40b34aae0
ThomasG77 <thomas_gratier@yahoo.fr>
2018-12-17 00:25:41 +0100

    Add left text block with Lorem_ipsum and refactor css to main.css and js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 import 'ol/ol.css';
+import './main.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import GeoJSON from 'ol/format/GeoJSON.js';
 import VectorLayer from 'ol/layer/Vector.js';
 import VectorSource from 'ol/source/Vector.js';
+import {Fill, Stroke, Style} from 'ol/style';
+import {unByKey} from 'ol/Observable';
 
-var vectorLayer = new VectorLayer({
-  source: new VectorSource({
-    url: 'data/srtm_1arc_hgt_zip.geojson',
-    format: new GeoJSON()
-  }),
-  renderMode: 'image'
-  // ,style: function(feature) {
-  //   style.getText().setText(feature.get('name'));
-  //   return style;
-  // }
+var vectorSource = new VectorSource({
+  url: 'data/srtm_1arc_hgt_zip.geojson',
+  format: new GeoJSON()
 });
 
-new Map({
+var vectorLayer = new VectorLayer({
+  source: vectorSource,
+  renderMode: 'image',
+  style:  new Style({
+    fill: new Fill({
+      color: [255, 255, 255, 0.0]
+    }),
+    stroke: new Stroke({
+      color: '#D24D57',
+      width: 1
+    })
+  })
+});
+
+var view = new View({
+  center: [10, -40],
+  zoom: 3,
+  maxZoom: 10
+});
+
+var map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url: 'http://{a-c}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'//'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       })
-    }),
-    vectorLayer
+    })
   ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+  view: view
 });
+
+map.addLayer(vectorLayer);
